5564b271a36360fa10c61afa77c06ffe1bff8f8b
Patrick Mutwiri <patwiri@gmail.com>
2018-11-20 10:16:00 +0300

    toast

--- app/src/main/java/com/example/pmutwiri/calctest/MainActivity.java
+++ app/src/main/java/com/example/pmutwiri/calctest/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText pass_word;
+    private Button button_sbm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("tag","onCreate");
         // test
-
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+        pass_word = (EditText)findViewById(R.id.editText);
+        button_sbm = (Button)findViewById(R.id.button);
+        button_sbm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public  void onClick(View v) {
+                        Toast.makeText(
+                                MainActivity.this, pass_word.getText(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+        );
     }
 
     public void onButtonClick(View v) {
-        EditText e1 = (EditText)findViewById(R.id.editText);
-        EditText e2 = (EditText)findViewById(R.id.editText2);
-        TextView t1 = (TextView)findViewById(R.id.textView);
-        int num1 = Integer.parseInt(e1.getText().toString());
-        int num2 = Integer.parseInt(e2.getText().toString());
-        int sum = num1+num2;
-        //Log.i()
-        t1.setText(Integer.toString(sum));
+
     }
 }
